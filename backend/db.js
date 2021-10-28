c49460b3e9ba3146c5bb6e7495d314e8c4d50bf3
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-10-28 18:17:15 +0530

    some backend setup is built

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+// Setup to connect the mongodb
+const mongoose = require('mongoose');
+
+mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+
+const connect_to_mongo =()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log('connect successfully to mongoDB')
+    })
+}
+
+module.exports = connect_to_mongo;

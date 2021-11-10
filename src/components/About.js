d2a50f311ef40be3483ef61625658acdbdfbdbaf
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-11-10 14:14:30 +0530

    React components and router setup is created

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            this is about page
+
+            
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            this is home page
+            
+        </div>
+    )
+}
+
+export default Home
